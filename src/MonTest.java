15f5f5eca0011c592cb6ff7a06797d4fa4c1e30c
root <root@fraise.(none)>
2013-09-10 16:37:12 +0200

    Ajout des tests unitaires pour l'histoire 2 Committers: Pierre Oudar, Simon Pruvost

--- src/MonTest.java
+++ src/MonTest.java
@@ -13,8 +13,70 @@ public class MonTest extends TestCase{
 		assertTrue((col == Yinsh.color.WHITE) || (col == Yinsh.color.BLACK));
 	}
 	
-	public void test2()
+	public void testVerifAnneauPlace()
 	{
+		//put_ring(String col, int line, Yinsh.color color)
+		Yinsh y = new Yinsh();
+		y.put_ring("A",1,Yinsh.color.WHITE);
+		assertTrue(plateau[0][1] == Yinsh.color.WHITE);
+	}
+	
+	public void testNombreAnneaux()
+	{
+		//On créé une nouvelle instance
+		Yinsh y = new Yinsh();
+		//On crée une variable qui va compter le nombre d'anneaux
+		int nb_anneaux=0;
+		
+		//On parcourt le tableau et on compte le nombre d'anneaux
+		for (int i = 0; i < y.plateau.length; i++) {
+			for (int j = 0; j < plateau[i].length; j++) {
+				if(y.plateau[i][j] != null) nb_anneaux++;
+			}
+		}
+		
+		//On test si après l'initialisation il n'y a pas d'anneau placé
+		assertTrue(nb_anneaux == 0);
+		
+		//On place un anneau
+		y.put_ring("A",1,Yinsh.color.WHITE);
+		
+		assertTrue(y.plateau[0][1] == Yinsh.color.WHITE);
+	}
+	
+	
+	public void testExceptionMauvaisesCoordonnees()
+	{
+		Yinsh y = new Yinsh();
+		
+		try {
+			
+			y.put_ring("C",3,Yinsh.color.WHITE);
+			
+		} catch (Exception e) {
+			
+			assertTrue(false);
+			
+		}
+		
+	}
+	
+	
+	public void testExceptionAnneauxMemeCouleurConsecutifs()
+	{
+		Yinsh y = new Yinsh();
+		
+		try {
+			
+			y.put_ring("A",1,Yinsh.color.WHITE);
+			y.put_ring("A",2,Yinsh.color.WHITE);
+			assertTrue(false);
+			
+		} catch (Exception e) {
+		
+			assertTrue(true);
+			
+		}
 		
 	}
 
